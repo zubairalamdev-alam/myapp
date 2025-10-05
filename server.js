e16d8bafedd97104cf282e486e78693739ed9e0e
Hasan <zubairalamdev@gmail.com>
2025-10-05 19:42:40 +0500

    added

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('pak army zindabad pak pak pakistani love Pakistan Pakistan zindabad≈'));
+app.get('/', (req, res) => res.send('pakistan army zindabad pak pak pakistani Hindustan Murdabad  love Pakistan Pakistan zindabad≈'));
 app.listen(port, () => console.log(`Listening on ${port}`));
 
